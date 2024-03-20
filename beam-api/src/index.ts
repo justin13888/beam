eb3a692be08c9bc43f130db9aa880bf5ed81800b
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-03-20 01:17:33 -0400

    beam-api: Add some elysia plugins

--- beam-api/src/index.ts
+++ beam-api/src/index.ts
@@ -1,6 +1,36 @@
 import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors'
+import { bearer } from '@elysiajs/bearer'
+import { serverTiming } from '@elysiajs/server-timing'
+import { swagger } from '@elysiajs/swagger'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .use(serverTiming())
+  .use(swagger({
+    documentation: {
+      tags: [
+        { name: 'Auth', description: 'Authentication' },
+        { name: 'User', description: 'User' },
+        { name: 'Admin', description: 'Admin' },
+      ]
+    }
+  }))
+
+  .use(bearer())
+  .get('/sign', ({ bearer }) => bearer, {
+    beforeHandle({ bearer, set }) {
+      if (!bearer) {
+        set.status = 400
+        set.headers[
+          'WWW-Authenticate'
+        ] = `Bearer realm='sign', error="invalid_request"`
+
+        return 'Unauthorized'
+      }
+    }
+  })
+  .get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`

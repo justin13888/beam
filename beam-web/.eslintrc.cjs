74fcf9abd63450265de21b585ccb07db22252b68
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-07-09 21:00:01 -0400

    Refactor beam-web with Vite

--- beam-web/.eslintrc.cjs
+++ beam-web/.eslintrc.cjs
@@ -1,51 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "prettier",
-        "plugin:astro/recommended",
-        "plugin:react/recommended"
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    "overrides": [
-        {
-            // Define the configuration for `.astro` file.
-            files: ["*.astro"],
-            // Allows Astro components to be parsed.
-            parser: "astro-eslint-parser",
-            // Parse the script in `.astro` as TypeScript by adding the following configuration.
-            // It's the setting you need when using TypeScript.
-            parserOptions: {
-                parser: "@typescript-eslint/parser",
-                extraFileExtensions: [".astro"],
-            },
-            processor: "astro/client-side-ts",
-            rules: {
-                // override/add rules settings here, such as:
-                // "astro/no-set-html-directive": "error"
-            },
-        },
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  },
 }
--- beam-web/README.md
+++ beam-web/README.md
@@ -1,23 +1,30 @@
-# Beam Web
-<!-- TODO: Redo this with Vite. Don't delete existing components -->
-Web interface for Beam. Built with Astro and React.
+# React + TypeScript + Vite
 
-## Features
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-WIP
+Currently, two official plugins are available:
 
-<!-- TODO -->
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-## Development
+## Expanding the ESLint configuration
 
-### Prerequisites
+If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
 
-- [Node.js](https://nodejs.org/en/download/)
-- [pnpm](https://pnpm.io/installation)
+- Configure the top-level `parserOptions` property like this:
 
-### Setup
-
-```sh
-pnpm install
-pnpm dev
+```js
+export default {
+  // other rules...
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+}
 ```
+
+- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
+- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
+- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list

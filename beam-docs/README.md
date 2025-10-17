c62ccd02926e6ef2e45c2ce1c1412437e6bab672
Justin Chung <20733699+justin13888@users.noreply.github.com>
2025-10-17 13:24:26 -0400

    Init beam-docs

---
+++ beam-docs/README.md
@@ -0,0 +1,12 @@
+# Beam Docs
+
+This is documentation website for Beam project.
+
+## Development
+
+```bash
+bun install
+bun dev
+```
+
+Development server running at <http://localhost:4321/>
---
+++ beam-docs/astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Beam',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/justin13888/beam' }],
+			sidebar: [ // TODO: Restructure as needed
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
